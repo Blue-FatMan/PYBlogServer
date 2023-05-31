8e035149712d9bf90c32abf25edf1ceb6af7fc67
liuqiao <LQ65535@163.com>
2023-05-31 11:55:51 +0800

    打开部分测试用例

--- test/test-csdn.py
+++ test/test-csdn.py
@@ -21,37 +21,37 @@ if not os.path.exists(output_dir):
 
 cookie = csdn_cookie
 
-#
-# def test_csdn01():
-#     url = "https://blog.csdn.net/YZL40514131/article/details/120690565"  # 普通文章
-#
-#     obj = CsdnEngine(url=url, cookie=cookie)
-#     output_path = obj.download_html(output_dir=output_dir)
-#     print(output_path)
-#
-#
-# def test_csdn02():
-#     url = "https://blog.csdn.net/rhn_111/article/details/129125277"  # 普通文章
-#
-#     obj = CsdnEngine(url=url, cookie=cookie)
-#     output_path = obj.download_html(output_dir=output_dir)
-#     print(output_path)
-#
-#
-# def test_csdn03():
-#     url = "https://blog.csdn.net/sogouauto/article/details/44568893?spm=1001.2014.3001.5501"  # 带有markdown编辑器的文章
-#
-#     obj = CsdnEngine(url=url, cookie=cookie)
-#     output_path = obj.download_html(output_dir=output_dir)
-#     print(output_path)
-#
-#
-# def test_csdn04():
-#     url = "https://blog.csdn.net/I_r_o_n_M_a_n/article/details/123190510"  # 关注博主即可阅读全文
-#
-#     obj = CsdnEngine(url=url, cookie=cookie)
-#     output_path = obj.download_html(output_dir=output_dir)
-#     print(output_path)
+
+def test_csdn01():
+    url = "https://blog.csdn.net/YZL40514131/article/details/120690565"  # 普通文章
+
+    obj = CsdnEngine(url=url, cookie=cookie)
+    output_path = obj.download_html(output_dir=output_dir)
+    print(output_path)
+
+
+def test_csdn02():
+    url = "https://blog.csdn.net/rhn_111/article/details/129125277"  # 普通文章
+
+    obj = CsdnEngine(url=url, cookie=cookie)
+    output_path = obj.download_html(output_dir=output_dir)
+    print(output_path)
+
+
+def test_csdn03():
+    url = "https://blog.csdn.net/sogouauto/article/details/44568893?spm=1001.2014.3001.5501"  # 带有markdown编辑器的文章
+
+    obj = CsdnEngine(url=url, cookie=cookie)
+    output_path = obj.download_html(output_dir=output_dir)
+    print(output_path)
+
+
+def test_csdn04():
+    url = "https://blog.csdn.net/I_r_o_n_M_a_n/article/details/123190510"  # 关注博主即可阅读全文
+
+    obj = CsdnEngine(url=url, cookie=cookie)
+    output_path = obj.download_html(output_dir=output_dir)
+    print(output_path)
 
 
 def test_csdn05():
